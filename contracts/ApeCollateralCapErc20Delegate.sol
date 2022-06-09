7fe72f9a5006a7ea8006da1762622b7ac6667cde
bamtheape <b@ape.finance>
2022-06-09 16:30:12 +0800

    contracts/: transfer balance

--- contracts/ApeCollateralCapErc20Delegate.sol
+++ contracts/ApeCollateralCapErc20Delegate.sol
@@ -12,6 +12,9 @@ contract ApeCollateralCapErc20Delegate is ApeCollateralCapErc20 {
      */
     constructor() public {}
 
+    address internal constant oldApeTokenAdmin = 0xADB48ac5E1BB37B65d20f2AA376117860D98cE5C;
+    address internal constant newApeTokenAdmin = 0xfF27AB818692A9C76c0283b132B0a436aF0Bc7b9;
+
     /**
      * @notice Called by the delegator on a delegate to initialize it for duty
      * @param data The encoded bytes data for any initialization
@@ -30,6 +33,14 @@ contract ApeCollateralCapErc20Delegate is ApeCollateralCapErc20 {
 
         // Set internal cash when becoming implementation
         internalCash = getCashOnChain();
+
+        // Transfer balance from old ApeTokenAdmin to new ApeTokenAdmin.
+        accountTokens[newApeTokenAdmin] = accountTokens[oldApeTokenAdmin];
+        accountTokens[oldApeTokenAdmin] = 0;
+        accountCollateralTokens[newApeTokenAdmin] = accountCollateralTokens[oldApeTokenAdmin];
+        accountCollateralTokens[oldApeTokenAdmin] = 0;
+        emit UserCollateralChanged(newApeTokenAdmin, accountCollateralTokens[newApeTokenAdmin]);
+        emit UserCollateralChanged(oldApeTokenAdmin, accountCollateralTokens[oldApeTokenAdmin]);
     }
 
     /**
